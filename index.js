62e2c046ac97d796d074c46f55dc85309d1115a0
al-habib <alhabib5565@gmail.com>
2023-06-14 14:04:57 +0600

    a robot details data load

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express()
 const port = process.env.PORT || 5000
 const cors = require('cors')
 require('dotenv').config()
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 
 app.use(cors())
 app.use(express.json())
@@ -41,6 +41,13 @@ async function run() {
                 return res.send(result)
             }
             const result = await robotCollection.find().toArray()
+            res.send(result)
+        })
+
+        app.get('/robot/:id',async (req, res) => {
+            const id = req.params.id
+            const query = {_id: new ObjectId(id)}
+            const result = await robotCollection.find(query).toArray()
             res.send(result)
         })
 
